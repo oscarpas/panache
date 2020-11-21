ac8ce7a3ce4320da73099b98520d902b41ebfe46
Oscar Pastarus <pastarus@gmail.com>
2020-11-21 17:59:09 +0100

    add: server side rendering

--- src/factory.tsx
+++ src/factory.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
-import { createStyleTag, removeStyleTag } from './sheet'
-
+import { serverSheet, createStyleTag, removeStyleTag } from './sheet'
 import { ThemeContext } from './theme'
 import { StyleGenerator, StyleObject } from './types'
 
@@ -20,23 +19,29 @@ const generateRandomId = (): string => Math.random().toString(36).replace('0.', 
 export function createElement(TargetComponent: string, styles: StyleObject | StyleGenerator ) {
   const PanacheComponent = React.forwardRef((props: React.ComponentProps<any>, ref) => {
     const theme = React.useContext(ThemeContext)
-    const [componentId] = React.useState(generateRandomId())
+    //const [componentId] = React.useState(generateRandomId())
+    // TODO variant id should probably include TargetComponent?
     const [componentVariationId] = React.useState(`pn${String(hash(JSON.stringify(props)))}`)
-
+    const isServerRendered = typeof window === 'undefined'
 
     const computedProps = {
       ...props,
       ref,
-      className: [componentId, componentVariationId, props.className].join(' ')
+      className: [componentVariationId, props.className].join(' ')
     }
 
     // styles can be a function or object
     const componentStyle = typeof styles === 'function' ? styles({ theme, ...props }): styles
 
+    // Add styles to server sheet if SSR
+    if (isServerRendered) serverSheet.add(styles, componentVariationId)
+
+    // Otherwise add style on first render
     React.useEffect(() => {
+      if (isServerRendered) return
       createStyleTag(componentStyle, componentVariationId)
       return () => removeStyleTag(componentVariationId)
-    }, [])
+    }, [isServerRendered])
 
     React.useEffect(() => {
       createStyleTag(componentStyle, componentVariationId)
--- src/sheet.ts
+++ src/sheet.ts
@@ -1,7 +1,40 @@
+import * as React from 'react'
 import { parse } from './parser'
 import { StyleObject } from './types'
+import ReactDOMServer from 'react-dom/server'
+
+class ServerSheet {
+  sheet: Object
+
+  constructor(sheet: Object | void) {
+    this.sheet = sheet || {}
+  }
+
+  add(styleObject: StyleObject, componentVariantId: string) {
+    this.sheet = {
+      ...this.sheet,
+      [componentVariantId]: parse(styleObject, componentVariantId)
+    }
+  }
+
+  get() {
+    return Object
+      .entries(this.sheet)
+      .map(([cssClass, cssRules]) =>
+        React.createElement('style', { className: cssClass, key: cssClass }, cssRules))
+  }
+}
+
+export const serverSheet = new ServerSheet()
+
+export const collectStyles = (App: any) => {
+  ReactDOMServer.renderToStaticMarkup(App)
+  return App
+}
 
 export function createStyleTag(styleObject: StyleObject, componentVariantId: string) {
+  // TODO check if tag with variant id already exists
+
   const styleString = parse(styleObject, componentVariantId)
   const parent = document.head
   const style = document.querySelector(`[panache-id="${componentVariantId}"]`)
